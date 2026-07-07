8bb4b29e4b839c7f35691a0d96f33558a65d30d9
faith-akiror <faithakiror95@gmail.com>
2026-07-07 10:06:12 +0300

    corrected black formating

--- backend/platform_admin/urls.py
+++ backend/platform_admin/urls.py
@@ -4,17 +4,9 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
-router.register(
-    r"announcements", views.AnnouncementViewSet, basename="announcement"
-)
-router.register(
-    r"feature-flags", views.FeatureFlagViewSet, basename="feature-flag"
-)
-router.register(
-    r"banners", views.BannerViewSet, basename="banner"
-)
-router.register(
-    r"system-messages", views.SystemMessageViewSet, basename="system-message"
-)
+router.register(r"announcements", views.AnnouncementViewSet, basename="announcement")
+router.register(r"feature-flags", views.FeatureFlagViewSet, basename="feature-flag")
+router.register(r"banners", views.BannerViewSet, basename="banner")
+router.register(r"system-messages", views.SystemMessageViewSet, basename="system-message")
 
 urlpatterns = [path("", include(router.urls))]
