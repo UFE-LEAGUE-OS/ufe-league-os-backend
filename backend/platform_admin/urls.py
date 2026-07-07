78d90fe5fc36d8177ef6f959f6f3f3977d16e20d
faith-akiror <faithakiror95@gmail.com>
2026-07-07 10:11:19 +0300

    Format platform admin urls

--- backend/platform_admin/urls.py
+++ backend/platform_admin/urls.py
@@ -7,6 +7,8 @@ router = DefaultRouter()
 router.register(r"announcements", views.AnnouncementViewSet, basename="announcement")
 router.register(r"feature-flags", views.FeatureFlagViewSet, basename="feature-flag")
 router.register(r"banners", views.BannerViewSet, basename="banner")
-router.register(r"system-messages", views.SystemMessageViewSet, basename="system-message")
+router.register(
+    r"system-messages", views.SystemMessageViewSet, basename="system-message"
+)
 
 urlpatterns = [path("", include(router.urls))]
